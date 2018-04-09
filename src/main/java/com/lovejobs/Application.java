dba036a44609cc3eeb423d18b90519bc5a06b45f
fengxin ushareit <fengxin@ushareit.com>
2018-04-09 18:27:54 +0800

    创建httpsclient支持工具类

--- src/main/java/com/lovejobs/Application.java
+++ src/main/java/com/lovejobs/Application.java
@@ -39,8 +39,8 @@ public class Application {
 	public Connector httpConnector() {
 		Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
+		connector.setPort(8081);
+		connector.setSecure(true);
 		connector.setRedirectPort(8443);
 		return connector;
 	}
